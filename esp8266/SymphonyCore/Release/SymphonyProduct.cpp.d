08b790853c8d50f0df8f6e46a8e6720b643a93aa
Carlo Miras <mirascarlo934@gmail.com>
2018-06-09 16:28:17 +0800

    first commit

---
+++ esp8266/SymphonyCore/Release/SymphonyProduct.cpp.d
@@ -0,0 +1,177 @@
+SymphonyProduct.cpp.o: ..\SymphonyProduct.cpp ..\SymphonyProduct.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Arduino.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdlib.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/ieeefp.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/newlib.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/config.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/reent.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/_types.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/_types.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/lock.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/cdefs.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/stdlib.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/alloca.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdio.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/types.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/types.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/stdio.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/string.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/string.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/math.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/stdlib_noniso.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/binary.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/esp8266_peri.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/c_types.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/twi.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Arduino.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/core_esp8266_features.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/pgmspace.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/ets_sys.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/c_types.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/eagle_soc.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/osapi.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/user_config.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/WCharacter.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/WString.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/pgmspace.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/HardwareSerial.h \
+ E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/inttypes.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Stream.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Print.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Printable.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/uart.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Esp.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Updater.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/flash_utils.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/MD5Builder.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/WString.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Stream.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/md5.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/debug.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\variants\generic/pins_arduino.h \
+ E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\variants\generic/common.h
+
+..\SymphonyProduct.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Arduino.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdlib.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/ieeefp.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/newlib.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/config.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/xtensa/config/core-isa.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/reent.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/_types.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/_types.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/lock.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/cdefs.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/stdlib.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/alloca.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdio.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/types.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/types.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/stdio.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/string.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/string.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/math.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/stdlib_noniso.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/binary.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/esp8266_peri.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/c_types.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/twi.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Arduino.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/core_esp8266_features.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/pgmspace.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/ets_sys.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/c_types.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/eagle_soc.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/include/osapi.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/user_config.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/WCharacter.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/WString.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/pgmspace.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/HardwareSerial.h:
+
+E:\sloeberv4\/arduinoPlugin/packages/esp8266/hardware/esp8266/2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/inttypes.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Stream.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Print.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Printable.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/uart.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Esp.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Updater.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/flash_utils.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/MD5Builder.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/WString.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Stream.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/md5.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/debug.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\variants\generic/pins_arduino.h:
+
+E:\sloeberv4\arduinoPlugin\packages\esp8266\hardware\esp8266\2.4.1\variants\generic/common.h:
