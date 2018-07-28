7ef05fd844a3c6c99786cb2a85f06ef660144c0e
Carlo Miras <mirascarlo934@gmail.com>
2018-07-28 21:26:42 +0800

    commit

---
+++ SymphonyIR/.ino.cpp
@@ -0,0 +1,25 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-06-19 19:01:47
+
+#include "Arduino.h"
+#include <mainhtml.h>
+#include <MqttUtil.h>
+#include <SpiffsUtil.h>
+#include <SymphonyCore.h>
+#include <SymphonyProduct.h>
+#include "Arduino.h"
+#include "SymphonyCore.h"
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+int WsCallback(uint8_t * payload, size_t length) ;
+attribStruct MyMqttCallback(attribStruct property, int scmd) ;
+void setup() ;
+void loop() ;
+
+#include "SymphonyIR.ino"
+
+
+#endif
