3377f5e0b81fcaa2977c14a69a4f11e61f9463b5
Carlo Miras <mirascarlo934@gmail.com>
2018-07-31 15:35:35 +0800

    initial commit

---
+++ SymphonyE131Tree/.ino.cpp
@@ -0,0 +1,25 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-07-31 15:26:39
+
+#include "Arduino.h"
+#include <E131.h>
+#include <FastLED.h>
+#include "SymphonyCore.h"
+int WsCallback(uint8_t * payload, size_t length) ;
+attribStruct MyMqttCallback(attribStruct property, int scmd) ;
+void setup() ;
+void loop() ;
+int handleCommand(int index, int cmd) ;
+void FillLEDsFromPaletteColors( uint8_t colorIndex) ;
+void ChangePalettePeriodically() ;
+void SetupTotallyRandomPalette() ;
+void SetupBlackAndWhiteStripedPalette() ;
+void SetupPurpleAndGreenPalette() ;
+
+#include "SymphonyE131Tree.ino"
+
+
+#endif
